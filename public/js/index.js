92a1a746371925264d0da89f664ca2e04d92f6fa
Alexwebjr <alexwebjr@gmail.com>
2022-06-27 10:15:42 -0400

    User Updating settings

--- public/js/index.js
+++ public/js/index.js
@@ -2,11 +2,17 @@
 import '@babel/polyfill';
 import { displayMap } from './mapbox';
 import { login, logout } from './login';
+import { updateSettings } from './updateSettings';
 
 //Element
 const mapBox = document.getElementById('map');
 const loginForm = document.querySelector('.form--login');
+const logInBtn = document.querySelector('.btn--login');
 const logOutBtn = document.querySelector('.nav__el--logout');
+const userDataForm = document.querySelector('.form-user-data');
+const userPasswordForm = document.querySelector('.form-user-password');
+const userDataSaveBtn = document.querySelector('.btn--save-settings');
+const userPasswordSaveBtn = document.querySelector('.btn--save-password');
 
 //Delegation
 if (mapBox) {
@@ -15,15 +21,45 @@ if (mapBox) {
   );
   displayMap(locations);
 }
-//On Submit
+//On Login Submit
 if (loginForm) {
   loginForm.addEventListener('submit', (e) => {
     e.preventDefault();
-
+    logInBtn.textContent = 'Waiting...'; //update Btn Text
     const email = document.getElementById('email').value;
     const password = document.getElementById('password').value;
     login(email, password);
+    logInBtn.textContent = 'Login'; //update Btn Text
   });
 }
 
 if (logOutBtn) logOutBtn.addEventListener('click', logout);
+
+//On UpdateMe Submit
+if (userDataForm)
+  userDataForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+    userDataSaveBtn.textContent = 'Updating...'; //update Btn Text
+    const name = document.getElementById('name').value;
+    const email = document.getElementById('email').value;
+    updateSettings({ name, email }, 'data');
+    userDataSaveBtn.textContent = 'Save settings'; //update Btn Text
+  });
+
+if (userPasswordForm)
+  userPasswordForm.addEventListener('submit', async (e) => {
+    e.preventDefault();
+    userPasswordSaveBtn.textContent = 'Updating...'; //update Btn Text
+    const passwordCurrent = document.getElementById('password-current').value;
+    const password = document.getElementById('password').value;
+    const passwordConfirm = document.getElementById('password-confirm').value;
+    await updateSettings(
+      { passwordCurrent, password, passwordConfirm },
+      'password'
+    );
+
+    userPasswordSaveBtn.textContent = 'Save password'; //update Btn Text
+    document.getElementById('password-current').value = '';
+    document.getElementById('password').value = '';
+    document.getElementById('password-confirm').value = '';
+  });
--- public/js/updateSettings.js
+++ public/js/updateSettings.js
@@ -2,6 +2,33 @@
 import axios from 'axios';
 import { showAlert } from './alerts';
 
-export const updateData(){
+export const updateSettings = async (data, type) => {
+  try {
+    const url =
+      type === 'password'
+        ? 'http://localhost:3000/api/v1/users/updateMyPassword'
+        : 'http://localhost:3000/api/v1/users/updateMe';
 
-}
+    console.log(url);
+    console.log(data);
+    console.log(type);
+
+    const res = await axios({
+      method: 'PATCH',
+      url,
+      data: data,
+    });
+
+    if (res.data.status === 'success') {
+      showAlert('success', `${type.toUpperCase()} updated successfully!`);
+      //reload
+      // window.setTimeout(() => {
+      //   location.reload();
+      // }, 1500);
+    }
+
+    console.log(res.data);
+  } catch (err) {
+    showAlert('error', err.response.data.message);
+  }
+};
