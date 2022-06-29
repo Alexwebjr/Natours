66c00f7c04a3dc28950d6103b0b6c4a5b6d0c5ff
Alexwebjr <alexwebjr@gmail.com>
2022-06-29 08:26:08 -0400

    Adding Emails

--- controllers/authController.js
+++ controllers/authController.js
@@ -10,7 +10,7 @@ const catchAsync = require('../helpers/catchAsync');
 
 const AppError = require('../helpers/appError');
 
-const sendEmail = require('../helpers/email');
+const Email = require('../helpers/email');
 
 //? GenerateToken
 const signToken = (id) =>
@@ -44,6 +44,9 @@ const createSendToken = (user, statusCode, res) => {
 //? SingUp
 exports.signup = catchAsync(async (req, res, next) => {
   const newUser = await User.create(req.body);
+
+  const url = `${req.protocol}://${req.get('host')}/me`; //built url
+  await new Email(newUser, url).sendWelcome();
 
   createSendToken(newUser, 201, res);
 });
@@ -171,19 +174,14 @@ exports.forgotPassword = catchAsync(async (req, res, next) => {
   // 2) Gnerate the random reset token
   const resetToken = user.createPasswordResetToken();
   await user.save({ validateBeforeSave: false });
-  // 3) Send it to user's email
-  const resetURL = `${req.protocol}://${req.get(
-    'host'
-  )}/api/v1/users/resetPassword/${resetToken}`;
 
-  const message = `Forgot your password? Submit a PATCH request with your new password and
-  passwordConfirm to: ${resetURL}.\nIf you didn't forget your password, please ignore this email!`;
+  // 3) Send it to user's email
   try {
-    await sendEmail({
-      email: user.email,
-      subject: 'Your password reset token (valid for 10 min)',
-      message,
-    });
+    const resetURL = `${req.protocol}://${req.get(
+      'host'
+    )}/api/v1/users/resetPassword/${resetToken}`;
+
+    await new Email(user, resetURL).sendPasswordReset();
 
     res.status(200).json({
       status: 'success',
